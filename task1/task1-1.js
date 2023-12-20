9dcfabfaf8cfecb6d69f0a7e5c25f927d0c2ccdd
Denis <olga.volotovich@mail.ru>
2023-12-20 15:02:46 +0100

    refactor: code style changes

--- task1/task1-1.js
+++ task1/task1-1.js
@@ -1,20 +1,25 @@
-const firstNumberPrompt = prompt("Pls enter a number", "");
-const secondNumberPrompt = prompt(
-  "Pls enter another number from 2 to 36 (that will be the number system)",
-  ""
-);
+const 
+      firstNumberPrompt = prompt("Pls enter a number", ""),
+      secondNumberPrompt = prompt(
+    "Pls enter another number from 2 to 36 (that will be the number system)",
+    ""
+  );
 
 function isNumberValid(value) {
   return (
     value !== null && value !== "" && !isNaN(+value) && Number.isFinite(+value)
   );
-};
-
-function isNumberSystemValid(value) {
-    return +value >= 2 && +value <= 36
 }
 
-if (isNumberValid(firstNumberPrompt) && isNumberValid(secondNumberPrompt) && isNumberSystemValid(secondNumberPrompt)) {
+function isNumberSystemValid(value) {
+  return +value >= 2 && +value <= 36;
+}
+
+if (
+  isNumberValid(firstNumberPrompt) &&
+  isNumberValid(secondNumberPrompt) &&
+  isNumberSystemValid(secondNumberPrompt)
+) {
   console.log((+firstNumberPrompt).toString(+secondNumberPrompt));
 } else {
   console.log("Incorrect!");
