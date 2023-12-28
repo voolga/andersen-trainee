035db20c090069145db57c23fa9103e3cd18e3e4
Denis <olga.volotovich@mail.ru>
2023-12-28 16:40:49 +0100

    refactor: ref task 1-1 according with code-review

--- task1/task1-1.js
+++ task1/task1-1.js
@@ -1,26 +1,25 @@
-const 
-      codefirstNumberPrompt = prompt("Pls enter a number", ""),
-      secondNumberPrompt = prompt(
+const codefirstNumberPrompt = prompt("Pls enter a number", "");
+const secondNumberPrompt = prompt(
     "Pls enter another number from 2 to 36 (that will be the number system)",
     ""
   );
 
-function isNumberValid(value) {
+function isValidInput(value) {
   return (
-    value !== null && value !== "" && !isNaN(+value) && Number.isFinite(+value)
+    value?.length && !isNaN(value) && isFinite(value)
   );
-}
+};
 
 function isNumberSystemValid(value) {
-  return +value >= 2 && +value <= 36;
-}
+  return value >= 2 && value <= 36;
+};
 
-if (
-  isNumberValid(firstNumberPrompt) &&
-  isNumberValid(secondNumberPrompt) &&
-  isNumberSystemValid(secondNumberPrompt)
-) {
+const isDataValid = isNumberValid(firstNumberPrompt) &&
+                    isNumberValid(secondNumberPrompt) &&
+                    isNumberSystemValid(secondNumberPrompt);
+
+if (isDataValid) {
   console.log((+firstNumberPrompt).toString(+secondNumberPrompt));
 } else {
   console.log("Incorrect!");
-}
+};
