73b8a3b5ea338f40b3f1eaf87284185a9feb8acb
Denis <olga.volotovich@mail.ru>
2023-12-28 01:17:35 +0100

    feat: add solution for task 2-2

---
+++ task2/task2-2.js
@@ -0,0 +1,39 @@
+function selectFromInterval(arr, start, end) {
+  function isRangeValid(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+      throw new Error("это не кот и он не умеет говорить мяу, дурень");
+    };
+  };
+
+  function isArrValid(array) {
+    if (!Array.isArray(array)) {
+      throw new Error("это не кот и он не умеет говорить мяу, дурень");
+    };
+    array.some((el) => {
+      if (typeof el !== "number") {
+        throw new Error("это не кот и он не умеет говорить мяу, дурень");
+      };
+    });
+  };
+
+  isArrValid(arr);
+  isRangeValid(start, end);
+
+  let min, max;
+  if (start < end) {
+    min = start;
+    max = end;
+  } else {
+    min = end;
+    max = start;
+  };
+  let result = [];
+
+  arr.filter((item) => {
+    return item >= min && item <= max;
+  });
+
+  return result;
+}
+
+console.log(selectFromInterval(['aaa'], 2, 3));
