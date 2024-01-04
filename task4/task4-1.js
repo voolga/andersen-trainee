f23cf2a0ca5606cfc8b2ede61f3a520214142284
Denis <olga.volotovich@mail.ru>
2024-01-04 22:48:16 +0100

    feat: add solutions for task 4-1

--- task4/task4-1.js
+++ task4/task4-1.js
@@ -1,47 +1,21 @@
-const infiniteCurry = () => {
-  const reduceValue = (args) =>
-    args.reduce((acc, a) => {
-      return acc + a;
-    });
+function concatStrings(initialString, separator) {
+  let resultString = initialString;
 
-  const next = (...args) => {
-    return (...x) => {
-      if (!x.length) {
-        return reduceValue(args);
-      }
-      return next(...args, ...x);
-    };
-  };
+  if (typeof separator !== 'string') {
+    separator = '';
+  }
 
-  return next();
-};
+  function concat(nextString) {
+    if (nextString === undefined) {
+      return resultString;
+    }
 
-const iSum = infiniteCurry();
+    if (typeof nextString === 'string') {
+      resultString += separator + nextString;
+    }
 
-console.log(iSum("first")("second")("third")("fourth", "fifth", "LOL")("")());
-console.log(iSum(1)(3, 4)(5, 6)(7, 8, 9)()); // 43
-console.log(iSum(3, 4, 5)()); // 12
+    return concat;
+  }
 
-// function concatStrings(initialString, separator = "") {
-//   let result = initialString;
-
-//   function concat(nextString) {
-//     // console.log(nextString);
-//     if (nextString === undefined) {
-//       return result;
-//     }
-
-//     if (typeof nextString === "string") {
-//       if (separator && typeof separator === "string") {
-//         result += separator + nextString;
-//       }
-//       result += nextString;
-//     }
-
-//     return concat;
-//   }
-
-//   return concat;
-// }
-
-// concatStrings("some-value")("333")(123n)();
+  return concat;
+}
