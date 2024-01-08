617f9fc8c757a8b1e4a7d5ceed3f9f6360d4507c
Denis <olga.volotovich@mail.ru>
2024-01-08 15:35:34 +0100

    feat: add solution to task 5-1

---
+++ task5/task5-1.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(data, prev) {
+    this.data = data;
+    this.prev = prev;
+  };
+};
+
+class Stack {
+  constructor(maxLength = 10, length = 0) {
+    this.last = null;
+    this.maxLength = maxLength;
+    this.length = length;
+  };
+
+  push(data) {
+    if (this.length === this.maxLength) {
+      throw new Error("stack is full");
+    }
+
+    this.last = new Node(data, this.last);
+    this.length++;
+  };
+
+  pop() {
+    if (this.last === null) {
+      throw new Error("stack is empty");
+    }
+
+    let result = this.last.data;
+
+    this.last = this.last.prev;
+    this.length--;
+
+    return result;
+  };
+
+  peek() {
+    if (this.last) {
+      return this.last.data;
+    } else {
+      return null;
+    }
+  };
+
+  isEmpty() {
+    return this.last === null;
+  };
+
+  toArray() {
+    let arrFromStack = [];
+    let currentNode = this.last;
+
+    while (currentNode) {
+      arrFromStack.push(currentNode.data);
+      currentNode = currentNode.prev;
+    };
+
+    return arrFromStack.reverse();
+  };
+
+  static fromIterable(iterable) {
+    if (iterable?.[Symbol.iterator] instanceof Function) {
+      const stack = new Stack(iterable.length);
+
+      for (let el of iterable) {
+        stack.push(el);
+      }
+
+      return stack;
+    } else {
+      throw new TypeError("passed data type is NOT iterable");
+    }
+  };
+};
