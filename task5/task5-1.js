07031495eadcc30bc6731e6960c08d90d3a2c1b1
Denis <olga.volotovich@mail.ru>
2024-01-08 18:38:36 +0100

    refactor: code style changes only

--- task5/task5-1.js
+++ task5/task5-1.js
@@ -2,15 +2,15 @@ class Node {
   constructor(data, prev) {
     this.data = data;
     this.prev = prev;
-  };
-};
+  }
+}
 
 class Stack {
-  constructor(maxLength = 10, length = 0) {
+  constructor(maxLength = 10) {
     this.last = null;
     this.maxLength = maxLength;
-    this.length = length;
-  };
+    this.length = 0;
+  }
 
   push(data) {
     if (this.length === this.maxLength) {
@@ -19,35 +19,31 @@ class Stack {
 
     this.last = new Node(data, this.last);
     this.length++;
-  };
+  }
 
   pop() {
     if (this.last === null) {
       throw new Error("stack is empty");
     }
 
-    let result = this.last.data;
+    const result = this.last.data;
 
     this.last = this.last.prev;
     this.length--;
 
     return result;
-  };
+  }
 
   peek() {
-    if (this.last) {
-      return this.last.data;
-    }
-
-    return null;
-  };
+      return this.last?.data ?? null;
+  }
 
   isEmpty() {
     return this.last === null;
-  };
+  }
 
   toArray() {
-    let arrFromStack = [];
+    const arrFromStack = [];
     let currentNode = this.last;
 
     while (currentNode) {
@@ -56,19 +52,19 @@ class Stack {
     }
 
     return arrFromStack.reverse();
-  };
+  }
 
   static fromIterable(iterable) {
-    if (iterable?.[Symbol.iterator] instanceof Function) {
-      const stack = new Stack(iterable.length);
-
-      for (let el of iterable) {
-        stack.push(el);
-      }
-
-      return stack;
-    } else {
+    if (!(iterable?.[Symbol.iterator] instanceof Function)) {
       throw new TypeError("passed data type is NOT iterable");
     }
-  };
+
+    const stack = new Stack(iterable.length);
+
+    for (let el of iterable) {
+      stack.push(el);
+    }
+
+    return stack;
+  }
 };
